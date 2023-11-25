e674eea9239ce5a0d4b9e3c2b212c97a2964fa89
NathanNeve <r0742822@student.thomasmore.be>
2023-11-25 13:51:00 +0100

    ssl context, empty url check

--- code/goodCoverScraper.py
+++ code/goodCoverScraper.py
@@ -1,6 +1,7 @@
 import sqlite3
 import urllib.request
 import os
+import ssl
 
 # Pad naar de database file
 conn = sqlite3.connect(
@@ -10,7 +11,7 @@ cursor = conn.cursor()
 # locatie gescrapete covers
 path_goede_covers = r"C:\goede_covers"
 
-# goede covers query
+# goede covers query → aantal = 6989
 goede_covers_query = """
 SELECT 
     JSON_EXTRACT(changes, '$.new_cover_url') AS new_cover_url,
@@ -21,12 +22,11 @@ FROM
 	JOIN cover_covernumber cn ON cl.id = cn.id
 WHERE 
 	JSON_EXTRACT(changes, '$.new_cover_url') IS NOT NULL
-    AND JSON_EXTRACT(changes, '$.new_cover_url') NOT LIKE '%cipal%'
-LIMIT 100;
+    AND JSON_EXTRACT(changes, '$.new_cover_url') NOT LIKE '%cipal%';
 """
 cursor.execute(goede_covers_query)
 goede_covers = cursor.fetchall()
-print(f"Dit zijn de goede covers: {goede_covers}")
+# print(f"Dit zijn de goede covers: {goede_covers}")
 
 conn.close()
 
@@ -34,20 +34,32 @@ conn.close()
 def downloadimages(covers, path):
     for cover in covers:
         url = cover[0]  # Extract de URL van de tuple
+        print(f"URL: {url}")  # debugging
+
+        if not url:  # checken of de url leeg is
+            print("Skipping empty URL")
+            continue
+
         number = cover[1]
         type = cover[2]
         file_name = f"{type}{number}.jpg"
         full_path = os.path.join(path, file_name)
 
         try:
-            urllib.request.urlretrieve(url, full_path)
+            # SSL context om verificatie te skippen
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+
+            with urllib.request.urlopen(url, context=ssl_context) as response:
+                with open(full_path, 'wb') as output_file:
+                    output_file.write(response.read())
+
             print(f"Downloaded image {file_name}: {url}")
-        except urllib.error.HTTPError as error:
-            if error.code == 410:
-                print(f"Image {file_name} is no longer available: {url}")
-            else:
-                print(f"Error downloading image {file_name}: {url}")
-                print(f"Error details: {error}")
+
+        except (urllib.error.HTTPError, urllib.error.URLError, ssl.SSLCertVerificationError) as error:
+            print(f"Error downloading image {file_name}: {url}")
+            print(f"Error details: {error}")
 
 
 downloadimages(goede_covers, path_goede_covers)
