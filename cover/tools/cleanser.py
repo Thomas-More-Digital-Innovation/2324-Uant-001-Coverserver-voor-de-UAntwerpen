3ac08dd912330065dafebdd467782bc3f5326a5a
nathanneve <nathanneve@live.be>
2023-12-01 15:48:37 +0100

    cleaner af

--- cover/tools/cleanser.py
+++ cover/tools/cleanser.py
@@ -1,25 +1,37 @@
-# file om google drive op te kuisen naar alleen png bestanden
-import pydrive
-
+# script to delete non jpg files from my drive
+import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+# Get the path to the directory containing the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+client_secrets_path = os.path.join(script_dir, 'client_secrets.json')
+
 # Authenticate and create the PyDrive client
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
 
-# Get the list of all files in your Google Drive
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-print(file_list)
-# # Define the file extension you want to keep (in this case, PNG)
-# allowed_extensions = ['png']
-#
-# # Iterate through each file and delete non-PNG files
-# for file in file_list:
-#     file_extension = file['title'].split('.')[-1].lower()
-#     if file_extension not in allowed_extensions:
-#         print(f"Deleting file: {file['title']}")
-#         file.Delete()
-#
-# print("Done deleting non-PNG files.")
+# Get the ID of the specific folder you want to clean up
+folder_name = 'slechte_covers'
+folder_query = f"title = '{folder_name}' and trashed=false"
+folder_list = drive.ListFile({'q': folder_query}).GetList()
+
+if not folder_list:
+    print(f"Folder '{folder_name}' not found.")
+    exit()
+
+# Get the ID of the folder
+folder_id = folder_list[0]['id']
+
+file_list = drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
+
+allowed_extensions = ['jpg']
+
+for file in file_list:
+    file_extension = file['title'].split('.')[-1].lower()
+    if file_extension not in allowed_extensions:
+        print(f"Deleting file: {file['title']}")
+        file.Delete()
+
+print("Done deleting non-JPG files.")
